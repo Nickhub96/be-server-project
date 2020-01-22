0a21d45233099582eaec7c736fcb340142ef1100
Nickhub96 <nick.farrer96@gmail.com>
2020-01-22 10:52:53 +0000

    fixed some errors

--- routes/api-router.js
+++ routes/api-router.js
@@ -5,12 +5,12 @@ const articlesRouter = require("../routes/article-router");
 const commentsRouter = require("../routes/comment-router");
 const { send405Error } = require("../errors/index");
 
-apiRouter.route("/").get((req, res, next) => {
-  res
-    .status(200)
-    .send({ msg: "This is where my endpoints will live" })
-    .all(send405Error);
-});
+apiRouter
+  .route("/")
+  .get((req, res, next) => {
+    res.status(200).send({ msg: "This is where my endpoints will live" });
+  })
+  .all(send405Error);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
