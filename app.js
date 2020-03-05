ede10056fa9cae87cc210ea2f937b6619a025bab
Nickhub96 <nick.farrer96@gmail.com>
2020-03-05 15:39:32 +0000

    still working on fixing the patch requests for votes

--- app.js
+++ app.js
@@ -14,14 +14,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
-
 app.use("/api", apiRouter);
 app.use("/*", routeErrors);
 app.use(customErrorHandler);
